3510679af086f07b093fbdf120a6fbc59cd7c538
slam <slam>
2016-05-11 17:41:03 +0100

    init

--- env/server.ts
+++ env/server.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import 'angular2-universal/polyfills';
+import 'angular2-universal/polyfills'; //replace zone
 import 'reflect-metadata';
 import * as express from 'express';
 import {
